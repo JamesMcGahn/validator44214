4e47139e12ee80aa843a6fc0d1b395fae2208eda
James McGahn <jmcgahn@gmail.com>
2023-05-20 10:04:46 -0400

    add login fn to authcontroller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,15 +2,38 @@ const User = require('../models/userModel');
 const signToken = require('../utils/signToken');
 const catchAsyncFn = require('../utils/catchAsyncFn');
 const { promisify } = require('util');
+const AppError = require('../utils/appError');
 
 const cookieOptions = {
   expires: new Date(
     Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
   ),
+  secure: process.env.NODE_ENV === 'production',
   httpOnly: true,
 };
 
-exports.login = catchAsyncFn(async (req, res, next) => {});
+exports.login = catchAsyncFn(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Missing email or password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.cookie('jwt', token, cookieOptions);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+  });
+});
 
 exports.register = catchAsyncFn(async (req, res, next) => {
   const newUser = await User.create({
@@ -21,9 +44,6 @@ exports.register = catchAsyncFn(async (req, res, next) => {
 
   const token = signToken(newUser._id);
 
-  if (process.env.NODE_ENV === 'production') {
-    cookieOptions.secure = true;
-  }
   res.cookie('jwt', token, cookieOptions);
 
   res.status(201).json({
