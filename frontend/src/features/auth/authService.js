dca19e899bf561f51c9364a25ef6ea1d269951d5
James McGahn <jmcgahn@gmail.com>
2023-05-20 11:45:41 -0400

    add auth store, login service, add authslice

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_URL = '/api/v1/auth';
+
+const login = async (userData) => {
+  const response = await axios.post(`${API_URL}/login`, userData);
+
+  return response.data.token;
+};
+
+const authService = {
+  login,
+};
+
+export default authService;
