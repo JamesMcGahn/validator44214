7705b7f6471b2987d73a0cf55cdbc0d86b55eab3
James McGahn <jmcgahn@gmail.com>
2023-05-21 16:24:09 -0400

    validate payload on validate page,
    - if edipayload add option to validate payload
    - styling

--- frontend/src/pages/Validate.jsx
+++ frontend/src/pages/Validate.jsx
@@ -1,68 +1,127 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Alert from 'react-bootstrap/Alert';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Spinner from '../components/ui/Spinner';
-import { Container } from 'react-bootstrap/';
+import { Container, Button, Row, Col } from 'react-bootstrap/';
 import { Card } from 'react-bootstrap';
+import { validate, reset } from '../features/edi/ediSlice';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import './validate.css';
 
 function Validate() {
-  const { isLoading, validatePayload, ediPayload } = useSelector(
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLoading, isSuccess, validatePayload, ediPayload } = useSelector(
     (state) => state.edi,
   );
+
+  useEffect(() => {
+    if (isSuccess && validatePayload) {
+      toast.success('File Validated - Review Report');
+      dispatch(reset());
+    }
+  }, [dispatch, isSuccess, validatePayload]);
+
+  const handleOnClick = () => {
+    if (ediPayload && !validatePayload) {
+      dispatch(validate(ediPayload));
+    }
+  };
 
   if (isLoading) {
     return <Spinner />;
   }
 
   return (
-    <Container>
-      {!validatePayload && (
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              There is not a file {!ediPayload ? 'uploaded' : 'validated'} yet.
-            </Card.Title>
-            <p>
-              Please go back to the
-              {!ediPayload
-                ? ' upload page and upload a x12 file.'
-                : ' read page and validate a file.'}
-            </p>
-          </Card.Body>
-        </Card>
-      )}
-      {validatePayload && validatePayload?.Status === 'success' && (
-        <Alert variant="success">
-          <Alert.Heading>The file was successfully validated.</Alert.Heading>
-          <p>
-            The file was successfully validated. It meets with the
-            specifcations.
-          </p>
-        </Alert>
-      )}
-      {validatePayload && validatePayload?.Status === 'error' && (
-        <>
-          {validatePayload.Details.map((error, i) => {
-            return (
-              <Alert variant="danger" key={`${i}-error`}>
-                <Alert.Heading>
-                  There was an error reading the file.
-                </Alert.Heading>
+    <Container id="validate-container">
+      <Container>
+        <Row>
+          <Col>
+            <h1>Validate File</h1>
+          </Col>
+          {ediPayload && ediPayload?.Result.Status === 'success' && (
+            <Col>
+              <Container className="d-flex justify-content-end" fluid>
+                {validatePayload ? (
+                  <Button onClick={() => navigate('/read')}> Read File</Button>
+                ) : (
+                  <Button onClick={handleOnClick}> Validate File </Button>
+                )}
+              </Container>
+            </Col>
+          )}
+        </Row>
+      </Container>
+      <Container id="validate-card-container">
+        {!validatePayload && (
+          <Card className="validate-card">
+            <Card.Body>
+              <Card.Title>
+                There is not a file {!ediPayload ? 'uploaded' : 'validated'}{' '}
+                yet.
+              </Card.Title>
+              <p>
+                {!ediPayload
+                  ? 'Please go back to the upload page and upload a x12 file.'
+                  : 'Please validate a file.'}
+              </p>
+              {ediPayload && ediPayload?.Result.Status === 'success' && (
+                <Container className="d-flex justify-content-end" fluid>
+                  <Button onClick={handleOnClick}> Validate File </Button>
+                </Container>
+              )}
+              {!ediPayload && (
+                <Container className="d-flex justify-content-end" fluid>
+                  <Button onClick={() => navigate('/')}> Upload Page </Button>
+                </Container>
+              )}
+            </Card.Body>
+          </Card>
+        )}
 
-                <p>Message: {error.Message}</p>
-                <hr />
-                <p className="mb-0">
-                  {error.Value
-                    ? `Value: ${error.Value}`
-                    : error.SegmentId
-                    ? `Segment ID: ${error.SegmentId}`
-                    : ''}
+        {validatePayload && validatePayload?.Status === 'success' && (
+          <Row>
+            <Col>
+              <Alert variant="success" className="validate-card">
+                <Alert.Heading>
+                  The file was successfully validated.
+                </Alert.Heading>
+                <p>
+                  The file was successfully validated. It meets with the
+                  specifcations.
                 </p>
               </Alert>
-            );
-          })}
-        </>
-      )}
+            </Col>
+          </Row>
+        )}
+        {validatePayload && validatePayload?.Status === 'error' && (
+          <Col>
+            <h3>{`There are ${validatePayload.Details.length} errors in the file.`}</h3>
+            {validatePayload.Details.map((error, i) => {
+              return (
+                <Row key={`${i}-error`}>
+                  <Alert variant="danger" className="validate-card">
+                    <Alert.Heading>
+                      There was an error reading the file.
+                    </Alert.Heading>
+
+                    <p>Message: {error.Message}</p>
+                    <hr />
+                    <p className="mb-0">
+                      {error.Value
+                        ? `Value: ${error.Value}`
+                        : error.SegmentId
+                        ? `Segment ID: ${error.SegmentId}`
+                        : ''}
+                    </p>
+                  </Alert>
+                </Row>
+              );
+            })}
+          </Col>
+        )}
+      </Container>
     </Container>
   );
 }
