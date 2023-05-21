ce724cfdaa15323bcd1a7cbe5fc2308c2a070a40
James McGahn <jmcgahn@gmail.com>
2023-05-21 16:58:12 -0400

    add validate handler
    add validate handler

--- backend/controllers/ediController.js
+++ backend/controllers/ediController.js
@@ -25,8 +25,19 @@ exports.readX12 = catchAsyncFn(async (req, res, next) => {
   });
 });
 
-exports.validateX12 = (req, res, next) => {
-  res.status(200).json({
-    status: 'success validate ',
+exports.validateX12 = catchAsyncFn(async (req, res, next) => {
+  const response = await axios({
+    method: 'post',
+    url: `${process.env.EDI_URL}validate?model=en_44_1`,
+    data: req.body,
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.EDI_API_KEY,
+    },
   });
-};
+  const validatedPayload = await response.data;
+
+  res.status(201).json({
+    status: 'success validated',
+    data: validatedPayload,
+  });
+});
